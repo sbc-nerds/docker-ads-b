5da1268e359d752f849b6a1e1763f0bd65bc3fc6
Jonathan Bradshaw <jonathan@iMac.home>
2019-01-06 11:10:47 -0500

    Updated to fix shiv error

--- adsbexchange-mlat/mlat-client-runner.sh
+++ adsbexchange-mlat/mlat-client-runner.sh
@@ -4,7 +4,7 @@ set -o errexit          # Exit on most errors (see the manual)
 #set -o errtrace         # Make sure any error trap is inherited
 set -o nounset          # Disallow expansion of unset variables
 #set -o pipefail         # Use last non-zero exit code in a pipeline
-set -o xtrace          # Trace the execution of the script (debug)
+#set -o xtrace          # Trace the execution of the script (debug)
 
 DUMP1090_SERVER='dump1090'
 DUMP1090_PORT='30005'
