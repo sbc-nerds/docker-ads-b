3ec581a6071f3e56dca5ab95dd55b006938e436c
Jonathan Bradshaw <jb@nrgup.net>
2019-01-07 09:11:26 -0500

    Updated to latest flightaware

--- flightaware/piaware-runner.sh
+++ flightaware/piaware-runner.sh
@@ -4,7 +4,7 @@ set -o errexit          # Exit on most errors (see the manual)
 #set -o errtrace         # Make sure any error trap is inherited
 set -o nounset          # Disallow expansion of unset variables
 #set -o pipefail         # Use last non-zero exit code in a pipeline
-set -o xtrace          # Trace the execution of the script (debug)
+#set -o xtrace          # Trace the execution of the script (debug)
 
 
 echo "Waiting for dump1090 to start up"
@@ -14,6 +14,7 @@ echo
 echo "FLIGHTAWARE_USERNAME=${FLIGHTAWARE_USERNAME}"
 echo "FLIGHTAWARE_PASSWORD=***"
 echo "FLIGHTAWARE_FEEDER_ID=${FLIGHTAWARE_FEEDER_ID}"
+echo "FLIGHTAWARE_GPS_HOST=${FLIGHTAWARE_GPS_HOST}"
 echo
 
 
@@ -33,10 +34,10 @@ else
     piaware-config "feeder-id" "${FLIGHTAWARE_FEEDER_ID}"
 fi
 
-# Fix issue with fa-mlat-client
-# The fa-mlat-client is run as "nobody" with most permissions dropped.
-# This causes issues with extracting to ~/.shiv (the default) so use /tmp instead.
-export SHIV_ROOT='/tmp'
+if [ -n "${FLIGHTAWARE_GPS_HOST}" ]; then
+    echo "GPS_HOST specified, forwarding port 2947 to ${FLIGHTAWARE_GPS_HOST}"
+    /usr/bin/socat -s TCP-LISTEN:2947,fork TCP:${FLIGHTAWARE_GPS_HOST}:2947 &
+fi
 
 piaware -plainlog
 
