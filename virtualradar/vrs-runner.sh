ee6c2ac36dc7c03c1a78728245649dfc62b2a504
Jonathan Bradshaw <jb@nrgup.net>
2019-01-07 12:45:25 -0500

    Fixed config volume

--- virtualradar/vrs-runner.sh
+++ virtualradar/vrs-runner.sh
@@ -6,20 +6,21 @@ set -o nounset          # Disallow expansion of unset variables
 #set -o pipefail         # Use last non-zero exit code in a pipeline
 set -o xtrace          # Trace the execution of the script (debug)
 
-DUMP1090_SERVER='dump1090'
-DUMP1090_PORT='30005'
+# DUMP1090_SERVER='dump1090'
+# DUMP1090_PORT='30005'
 
 
-echo "Waiting for dump1090 to start up"
-sleep 5s
+# echo "Waiting for dump1090 to start up"
+# sleep 5s
 
-echo "Ping test to dump1090"
-ping -c 3 "${DUMP1090_SERVER}"
+# echo "Ping test to dump1090"
+# ping -c 3 "${DUMP1090_SERVER}"
 
 export MONO_PATH="/opt/vrs:${MONO_PATH:=}"
 
 # https://www.mono-project.com/docs/advanced/runtime/logging-runtime-events/
-export MONO_LOG_LEVEL='info'
+# logging level are "error", "critical", "warning", "message", "info", and "debug"
+export MONO_LOG_LEVEL='message'
 export MONO_LOG_MASK='dll,cfg'
 
 if [ ! -f "/root/.local/share/VirtualRadar/Configuration.xml" ]; then
